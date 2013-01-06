b990e6d294b7435ba94f997367e36f10a59d6358
benastan <bennyjbergstein@gmail.com>
2013-01-05 22:18:14 -0800

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Splitzies::Application.config.secret_token = 'a6bf65f5766451a87e0e402b35e31f7139a149e34254434d294be4e3c072265f29287223e7de47d468489afcd97013dacb19d72c67005dd231c79791a912dc41'
