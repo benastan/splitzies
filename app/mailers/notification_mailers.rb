9b697944fe5622160fbb022c452be34960beb838
benastan <bennyjbergstein@gmail.com>
2013-02-16 18:22:14 -0800

    Fixing stupid bug.

--- app/mailers/notification_mailers.rb
+++ app/mailers/notification_mailers.rb
@@ -4,6 +4,6 @@ class NotificationMailers < ActionMailer::Base
   def notifications_digest user
     @user = user
     @roommate_notifications = @user.roommate_notifications
-    mail(from: "#{@user.household.nickname} Expenses (Splitzies.com) <team@benbergstein.mailgun.org>", to: "#{@user.first_name} #{@user.last_name} <#{@user.email}>", subject: "#{user.notify_every)
+    mail(from: "#{@user.household.nickname} Expenses (Splitzies.com) <team@benbergstein.mailgun.org>", to: "#{@user.first_name} #{@user.last_name} <#{@user.email}>", subject: "Expense Update")
   end
 end
